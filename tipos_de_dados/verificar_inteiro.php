ecc014444b5861141092a249bfcec13a59b54a49
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 16:42:31 -0300

    verificando numeros inteiros

--- tipos_de_dados/verificar_inteiro.php
+++ tipos_de_dados/verificar_inteiro.php
@@ -1,1 +1,13 @@
 <?php
+
+    $intNumber = 5;
+    $stringNumber = "5";
+
+    if(is_int($intNumber)){
+        echo "É inteiro! <br>";
+    }
+
+    if(!is_int($stringNumber)){
+        echo "Não é inteiro <br>";
+    }
+?>
