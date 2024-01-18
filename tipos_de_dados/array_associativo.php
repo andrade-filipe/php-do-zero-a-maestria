f4e1f5d11088df444b6132b0c75a3291e4f49577
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 17:39:10 -0300

    exercicio 7

--- tipos_de_dados/array_associativo.php
+++ tipos_de_dados/array_associativo.php
@@ -1,6 +1,6 @@
 <?php
 
-$arr = ['nome'=> 'Bob', 'patas'=> 4, 'cor'=>'Branco'];
+$arr = ['nome' => 'Bob', 'patas' => 4, 'cor' => 'Branco'];
 
 echo $arr['cor'];
 echo "<br>";
@@ -11,15 +11,32 @@ echo "<br>";
 //exercicio 6
 
 $carros = [
-    'modelo'=>'Corolla',
-    'montadora'=>'Toyota',
-    'ano'=>2023,
-    'kilometragem'=>30000,
-    ];
+    'modelo' => 'Corolla',
+    'montadora' => 'Toyota',
+    'ano' => 2023,
+    'kilometragem' => 30000,
+];
 
 echo $carros['ano'];
 echo "<br>";
 echo $carros['kilometragem'];
 echo "<br>";
 print_r($carros);
+echo "<br>";
+echo "<br>";
+
+//exercicio 7
+
+$pessoa = [
+    'nome' => 'Filipe',
+    'idade' => 21,
+    'peso' => 89,
+    'estado_civil' => 'casado com sarah',
+];
+
+$maioridade_por_lei = 18;
+
+if ($pessoa['idade'] > $maioridade_por_lei) {
+    echo "é de maior por lei <br>";
+}
 ?>
