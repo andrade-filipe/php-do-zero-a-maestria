cfeb02442c76b6ca0cd267d5ef03756eee85e246
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 20:27:45 -0300

    exercicio 10 e ordem de prioridade de operadores

---
+++ expressoes_e_operadores/ordem_operadores.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 3 + 2 * 5;
+echo "<br>";
+echo (3 + 2) * 5;
+echo "<br>";
+echo 5+2/10;
+echo "<br>";
+
+$a = 5;
+$b = 2;
+$c = 10;
+
+$d = $a * $b * $c;
+
+echo $d;
+echo "<br>";
+
+//exercicio 10
+
+$e = $a / $c * $d - $b;
+
+echo $e;
+
+?>
