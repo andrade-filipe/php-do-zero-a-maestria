723f2f9648585a23c2ca0e0634e07dced9dcb8a7
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 16:04:08 -0300

    first commit

---
+++ case_sensitive.php
@@ -0,0 +1,16 @@
+<?php
+
+    //not case sensitive
+    echo "teste <br>";
+    ECHO "teste 2 <br>";
+    eChO "teste 3 <br>";
+
+    //case sensitive
+    $nome = "Matheus";
+    $NOME = "Filipe";
+
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
+
+?>
