a2ba2502e84becc64e983a6fa7e6999c46adbe6b
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 19:30:00 -0300

    variavel de referencia

---
+++ variaveis/variavel_de_referencia.php
@@ -0,0 +1,30 @@
+<?php
+class ValueHolder
+{
+    public $value;
+}
+
+$var = new ValueHolder();
+$var->value = 10;
+
+$sync = &$var;
+
+echo $var->value;
+echo "<br>";
+echo $sync->value;
+echo "<br>";
+
+$sync->value = 15;
+
+echo $var->value;
+echo "<br>";
+echo $sync->value;
+echo "<br>";
+
+$var->value = 20;
+
+echo $var->value;
+echo "<br>";
+echo $sync->value;
+echo "<br>";
+?>
