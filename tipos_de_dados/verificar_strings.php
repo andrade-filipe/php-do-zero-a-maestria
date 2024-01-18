ca8b489bfaec75f47edf8e7c8bf2c069efebfaa8
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 17:02:53 -0300

    verificando string

---
+++ tipos_de_dados/verificar_strings.php
@@ -0,0 +1,13 @@
+<?php
+
+    $str = "Filipe";
+    $num = 4;
+
+    if(is_string($str)){
+        echo "$str é uma string <br>";
+    }
+
+    if(!is_string($num)){
+        echo "$num não é uma string <br>";
+    }
+?>
