b652c31c87ea78fc468dc8c323442b687314452d
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 17:23:18 -0300

    tipo de dado: array

--- tipos_de_dados/arrays.php
+++ tipos_de_dados/arrays.php
@@ -2,6 +2,12 @@
 
 $arr = [1,2,3,4];
 
+print_r($arr); //função especial para printar arrays
 
+echo "<br>";
+echo $arr[0]; //pode-se utilizar o echo nesse caso
+echo "<br>";
 
+$arrMixed = ["Filipe", 30, 1.4];
+print_r($arrMixed) //array com vários tipos de dados é aceito em php
 ?>
