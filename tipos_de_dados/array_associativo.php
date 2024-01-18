8f6da6a0b2e4171db672a41fb4f5ba97de57b7c5
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 17:30:22 -0300

    exercicio 6

--- tipos_de_dados/array_associativo.php
+++ tipos_de_dados/array_associativo.php
@@ -5,5 +5,14 @@ $arr = ['nome'=> 'Bob', 'patas'=> 4, 'cor'=>'Branco'];
 echo $arr['cor'];
 echo "<br>";
 print_r($arr);
+echo "<br>";
 
+//exercicio 6
+
+$carros = ['modelo'=>'Corolla', 'montadora'=>'Toyota', 'ano'=>2023, 'kilometragem'=>30.000];
+
+echo $carros['ano'];
+echo "<br>";
+echo $carros['kilometragem'];
+echo "<br>";
 ?>
