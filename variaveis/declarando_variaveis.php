02a0d61b178cf10c313b5008b41af3cd58ddd99b
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 19:12:38 -0300

    exercicio 8

---
+++ variaveis/declarando_variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+    $teste = "Texto";
+
+    echo $teste;
+
+    $num = 10;
+
+    echo "<br>";
+    echo $num;
+    echo "<br>";
+
+    $velocidadeMaxima = 100; // camel case
+    echo $velocidadeMaxima;
+?>
