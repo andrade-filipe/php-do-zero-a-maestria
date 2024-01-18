e30f39743bbfe527a02282c7419164f90d98ac89
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 17:13:26 -0300

    tipo de dado: boolean

---
+++ tipos_de_dados/booleanos.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo true;
+    echo "<br>";
+    echo false;
+    echo "<br>";
+
+    if (true) {
+        echo "é verdadeiro <br>";
+    }
+    if (5 > 2) {
+        echo "também é verdadeiro <br>";
+    }
+
+?>
