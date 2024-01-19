baee0d180132ffe0a33521ea98ad5d25033b7774
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 16:31:03 -0300

    include_once e require_once

---
+++ inclusao_de_codigo/require_e_include_once/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    include_once "teste.php";
+    require_once "teste2.php";
+
+    include_once "fakefile.php";
+    require_once "fakefile.php"; //se comentar aparece o texto abaixo
+
+?>
+
+<h1>testando codigo</h1>
---
+++ inclusao_de_codigo/require_e_include_once/teste2.php
@@ -0,0 +1,1 @@
+<h1>REQUIRE ONCE</h1>
