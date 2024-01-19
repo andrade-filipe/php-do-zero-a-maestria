ba33908140d6dc4ed5dbe0d5578f245da786b531
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:45:39 -0300

    operadores especiais e booleanos, exercicio 15

---
+++ expressoes_e_operadores/operadores_especiais.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 0;
+echo ++$x; //se colocar ++ antes, ele printa a variável depois de adicionar
+echo "<br>";
+
+$x++;
+echo $x;
+echo "<br>";
+echo $x++; // printa a variável depois adiciona
+echo "<br>";
+echo $x;
+echo "<br>";
+
+$y = 10;
+
+echo --$y;
+?>
