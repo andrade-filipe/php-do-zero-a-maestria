6fb5976b8f283035f4e18d8a9e19ccd7342aa97f
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:12:28 -0300

    exercicio 12

---
+++ expressoes_e_operadores/operadores_aritmeticos.php
@@ -0,0 +1,17 @@
+<?php
+
+$soma = 5 + 5;
+$subtracao = 5 - 5;
+$multiplicacao = 5 * 5;
+$divisao = 5 / 5;
+
+echo $soma;
+echo "<br>";
+echo $subtracao;
+echo "<br>";
+echo $multiplicacao;
+echo "<br>";
+echo $divisao;
+echo "<br>";
+
+?>
