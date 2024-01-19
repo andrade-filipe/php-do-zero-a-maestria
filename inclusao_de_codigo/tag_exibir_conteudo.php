4c05739130fffa3936ab38e7a23178ca81bd71f5
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 16:43:08 -0300

    tag de exibição de conteudo

---
+++ inclusao_de_codigo/tag_exibir_conteudo.php
@@ -0,0 +1,13 @@
+<?php
+$nome = "Filipe";
+?>
+
+<form action="">
+    <div>
+        <!-- completa o campo automaticamente -->
+        <input type="text" value="<?= $nome?>">
+    </div>
+    <div>
+        <input type="submit" value="submit">
+    </div>
+</form>
