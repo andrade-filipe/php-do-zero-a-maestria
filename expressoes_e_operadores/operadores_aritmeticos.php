801065b6e87f4a2d063540e5365b0c60543fcbf1
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:17:58 -0300

    exercicio 13

--- expressoes_e_operadores/operadores_aritmeticos.php
+++ expressoes_e_operadores/operadores_aritmeticos.php
@@ -4,6 +4,8 @@ $soma = 5 + 5;
 $subtracao = 5 - 5;
 $multiplicacao = 5 * 5;
 $divisao = 5 / 5;
+$modulo = 12 % 4;
+$moduloComResto = 12 % 5;
 
 echo $soma;
 echo "<br>";
@@ -13,5 +15,9 @@ echo $multiplicacao;
 echo "<br>";
 echo $divisao;
 echo "<br>";
-
+echo "<br>";
+echo $modulo;
+echo "<br>";
+echo $moduloComResto;
+echo "<br>";
 ?>
