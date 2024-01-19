1482a0a76253cd5a9a8dfdf5d1d0be3f4ac2b49d
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:47:15 -0300

    exercicio 15

--- expressoes_e_operadores/operadores_booleanos.php
+++ expressoes_e_operadores/operadores_booleanos.php
@@ -16,4 +16,18 @@ if(3 !== "3"){
     echo "3 !== 3 <br>";
 }
 
+echo "<br>";
+
+$a = 6;
+$b = 4;
+$c = 6;
+
+if($a == $b){
+    echo "A igual B <br>";
+}
+
+if($a == $c){
+    echo "A igual C <br>";
+}
+
 ?>
