ea05b2f2efe669b9de799a788b1d449c30a56b09
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:30:25 -0300

    concatenação

---
+++ expressoes_e_operadores/concatenacao.php
@@ -0,0 +1,10 @@
+<?php
+
+$saudacao = "Bom dia!";
+$nome = "Filipe";
+$sobrenome = "Andrade";
+
+echo "$saudacao $nome $sobrenome como posso ajudar? <br>";
+
+echo $saudacao . " " . $nome . " " . $sobrenome . " " . "como posso ajudar? <br>";
+?>
