9e50afc4424902e0aedd374fc907c22ebb53283c
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 22:01:48 -0300

    exercicio 16

--- expressoes_e_operadores/operadores_booleanos.php
+++ expressoes_e_operadores/operadores_booleanos.php
@@ -1,19 +1,11 @@
 <?php
-
+//igual e diferente
 if(3 == 3){
     echo "3 == 3 <br>";
 }
 
 if(3 != 4){
     echo "3 != 4 <br>";
-}
-
-if(3 === 3){
-    echo "3 === 3 <br>";
-}
-
-if(3 !== "3"){
-    echo "3 !== 3 <br>";
 }
 
 echo "<br>";
@@ -28,6 +20,36 @@ if($a == $b){
 
 if($a == $c){
     echo "A igual C <br>";
+}
+
+echo "<br>";
+
+// identico a e não identico a
+
+if(3 === 3){
+    echo "3 === 3 <br>";
+}
+
+if(3 !== "3"){
+    echo "3 !== 3 <br>";
+}
+
+echo "<br>";
+
+$x = 5;
+$y = 3;
+
+if($x == $y){
+    echo "5 == 3 <br>";
+}
+if($x != $y){
+    echo "5 != 3 <br>";
+}
+if($x === $y){
+    echo "5 === 3 <br>";
+}
+if($x !== $y){
+    echo "5 !== 3 <br>";
 }
 
 ?>
