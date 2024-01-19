2806ac043d4f81094e7231420ada2f8667d70c6f
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 00:01:37 -0300

    operadores logicos

---
+++ expressoes_e_operadores/operadores_logicos.php
@@ -0,0 +1,25 @@
+<?php
+
+if(true && true){
+    echo "true && true <br>";
+}
+if(true && false){
+    echo "true && false <br>";
+}
+if(false && false){
+    echo "false && false <br>";
+}
+
+echo "<br>";
+
+if(true || true){
+    echo "true || true <br>";
+}
+if(true || false){
+    echo "true || false <br>";
+}
+if(false || false){
+    echo "false || false <br>";
+}
+
+?>
