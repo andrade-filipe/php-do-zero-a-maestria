fdb7a3efa8ba53dfef8c04e6312a6bf49a3ef6e5
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-18 21:20:36 -0300

    exponenciação

--- expressoes_e_operadores/operadores_aritmeticos.php
+++ expressoes_e_operadores/operadores_aritmeticos.php
@@ -6,6 +6,7 @@ $multiplicacao = 5 * 5;
 $divisao = 5 / 5;
 $modulo = 12 % 4;
 $moduloComResto = 12 % 5;
+$exponencial = 5 ** 5;
 
 echo $soma;
 echo "<br>";
@@ -16,8 +17,12 @@ echo "<br>";
 echo $divisao;
 echo "<br>";
 echo "<br>";
+//exercicio 13
 echo $modulo;
 echo "<br>";
 echo $moduloComResto;
+echo "<br>";
+echo "<br>";
+echo $exponencial;
 echo "<br>";
 ?>
