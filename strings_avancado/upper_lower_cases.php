8ad1c3e06a8ac5b9669425bfceeaea2d311805bf
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 21:58:13 -0300

    ucfirst e ucwords

--- strings_avancado/upper_lower_cases.php
+++ strings_avancado/upper_lower_cases.php
@@ -7,5 +7,16 @@ $strUpper = "CAIXA ALTA";
 echo strtoupper($strLower);
 echo "<br>";
 echo strtolower($strUpper);
+echo "<br>";
+
+$frase = "testando case da primeira letra";
+
+echo ucfirst($frase);
+echo "<br>";
+
+$frase2 = "todas as palavras começando com maiuscula";
+
+echo ucwords($frase2);
+echo "<br>";
 
 ?>
