af39af8e1e0d80e362e968494878704beb33cbec
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 23:12:42 -0300

    matriz

---
+++ arrays/array_multidimencional.php
@@ -0,0 +1,19 @@
+<?php
+
+$arr = [
+    [1,2,3],
+    [4,5,6]
+];
+
+print_r($arr);
+echo "<br>";
+
+echo $arr[0][2];
+echo "<br>";
+echo $arr[1][1];
+echo "<br>";
+echo count($arr);
+echo "<br>";
+echo count($arr[0]);
+echo "<br>";
+?>
