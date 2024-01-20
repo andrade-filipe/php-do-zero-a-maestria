86fdff92fe5bb1d405f6f361389cd85f40f2c993
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-19 22:30:54 -0300

    strstr, substr + strpos

--- strings_avancado/sub_strings.php
+++ strings_avancado/sub_strings.php
@@ -3,26 +3,39 @@
 $string = "String muito legal";
 
 $muito = substr($string, 7, 5);
-
 echo "$muito <br>";
 
 $restoDaString = substr($string, 7);
-
 echo "$restoDaString <br>";
 
 $negativo= substr($string, 7, -3);
-
 echo "$negativo <br>";
 
 $inverter = strrev($string);
-
 echo "$inverter <br>";
 
 echo str_repeat($string, 5);
-
 echo "<br>";
 
 $arr = explode(" ",$string);
-
 print_r($arr);
+echo "<br>";
+
+echo implode(",",$arr);
+echo "<br>";
+
+$testeStrPos = strpos($string, $muito);
+echo $testeStrPos;
+echo "<br>";
+
+//strrpos() encontra o ultimo indice ao invés do primeiro.
+
+$extrairSemSaberIndice = substr($string, strpos($string, "legal"), 5);
+
+echo $extrairSemSaberIndice;
+echo "<br>";
+
+$restoDaString2 = strstr($string, "muito");
+
+echo $restoDaString2
 ?>
