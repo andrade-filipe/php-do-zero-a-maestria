c2219ebb15fa4675f62d2fadcbdcab85a05c2265
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-21 00:18:46 -0300

    funcoes de ordenacao

---
+++ arrays/ordenacao.php
@@ -0,0 +1,34 @@
+<?php
+
+$arr = [30,10,1,3,6,20,55,65,0,4,3,2,11];
+
+sort($arr);
+
+print_r($arr);
+echo "<br>";
+
+$arrReverse = [30,10,1,3,6,20,55,65,0,4,3,2,11];
+
+rsort($arrReverse);
+print_r($arrReverse);
+echo "<br>";
+echo "<br>";
+
+$arrAssoc = ["a" => "elemento1", "zebra" => "animal", "chave" => "valor", "ordenacao" => "order"];
+
+asort($arrAssoc); //por valor
+print_r($arrAssoc);
+echo "<br>";
+
+print_r($arrAssoc); //por valor reverso
+echo "<br>";
+echo "<br>";
+
+ksort($arrAssoc); //por chave
+print_r($arrAssoc);
+echo "<br>";
+
+krsort($arrAssoc); //por chave reverso
+print_r($arrAssoc);
+echo "<br>";
+?>
