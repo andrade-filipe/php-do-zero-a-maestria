7695610ab51515db9eb09ae2b2244495c0295c66
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-25 23:43:27 -0300

    function self

--- oop/constante.php
+++ oop/constante.php
@@ -4,6 +4,9 @@
 
         public const CHAVE_API = "1F2J3H4J4JN3J34K34";
 
+        function mostrarConstante(){
+            echo self::CHAVE_API;
+        }
     }
 
     $constante = new Constante;
