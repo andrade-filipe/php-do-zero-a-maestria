be459584d6b52e42c044c083ec73f13435feb381
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-27 00:53:48 -0300

    ultimo exercicio da seção

---
+++ oop/exercicio_passageiro.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Passenger{
+        private $name;
+        private $age;
+        private $seatNumber;
+
+        function __construct($name,$age,$seatNumber){
+            $this->name = $name;
+            $this->age = $age;
+            $this->seatNumber = $seatNumber;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function getAge(){
+            return $this->age;
+        }
+
+        public function getSeatNumber(){
+            return $this->seatNumber;
+        }
+
+        public function setSeatNumber($seatNumber){
+            $this->seatNumber = $seatNumber;
+        }
+    }
