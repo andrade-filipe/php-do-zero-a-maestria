68db41df71084c9ac113ad380ee618f82d036e16
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-02 16:52:14 -0300

    prieira query utilizando mysqli

--- conexao_bd/conexao.php
+++ conexao_bd/conexao.php
@@ -1,7 +1,11 @@
 <?php
     $host = "localhost";
-    $user = "andrade";
-    $password = "senha exemplo";
+    $user = "root";
+    $password = "Root*3306";
     $db = "curso_php";
     $connection = new mysqli($host, $user, $password, $db);
+
+    if($connection->connect_errno){
+        echo "Erro: " . $connection->connect_error;
+    }
 ?>
---
+++ conexao_bd/executar_query.php
@@ -0,0 +1,9 @@
+<?php
+    include_once "conexao.php";
+
+    $query = "SELECT * FROM itens";
+
+    $result = $connection->query($query);
+
+    print_r($result);
+?>
