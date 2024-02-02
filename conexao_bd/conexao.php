6d63920af63423988124665a96e7638476c86409
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-02 16:26:59 -0300

    organizando

--- conexao_bd/conexao.php
+++ conexao_bd/conexao.php
@@ -1,3 +1,7 @@
 <?php
-    $connection = new mysqli("localhost", "andrade", "senha exemplo", "curso_php");
+    $host = "localhost";
+    $user = "andrade";
+    $password = "senha exemplo";
+    $db = "curso_php";
+    $connection = new mysqli($host, $user, $password, $db);
 ?>
