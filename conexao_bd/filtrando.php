0cf8f5675e02cdc53dffdf084be83b385f923df5
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-03 16:00:44 -0300

    filtrando somente uma linha da tabela

--- conexao_bd/filtrando.php
+++ conexao_bd/filtrando.php
@@ -14,4 +14,16 @@
     $data = $result->fetch_all(); //transforma em um array
 
     print_r($data);
+    echo "<br>";
+
+    $stmt2 = $connection->prepare("SELECT * FROM itens WHERE id = ?");
+    $stmt2->bind_param("i", $id);
+    $stmt2->execute();
+
+    $result = $stmt2->get_result();
+    $item = $result->fetch_row();
+
+    print_r($item);
+
+    $connection->close();
 ?>
