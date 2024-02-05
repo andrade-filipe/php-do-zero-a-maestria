77281b7b7547077f4db53bf895f918e467d9bbff
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-04 23:32:54 -0300

    create carDAO

--- dao/dao_impl/CarDAO.php
+++ dao/dao_impl/CarDAO.php
@@ -3,12 +3,25 @@
     include_once("../model/Car.php");
 
     class CarDAO implements CarDAO{
+        private $connection;
+
+        public function __construct(PDO $connection){
+            $this -> connection = $connection;
+        }
+
         public function findAll(){
 
         }
 
         public function create(Car $car){
 
+            $stmt = $this -> connection -> prepare("INSERT INTO cars (brand,km,color) VALUES (:brand, :km, :color)");
+
+            $stmt -> bindParam(":brand", $car -> getBrand());
+            $stmt -> bindParam(":color", $car -> getColor());
+            $stmt -> bindParam(":km", $car -> getKm());
+
+            $stmt -> execute();
         }
     }
 ?>
