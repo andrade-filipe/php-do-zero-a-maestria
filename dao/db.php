fcf47da67b73c62a4933d3dd6b903550da3e12c3
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-04 22:59:00 -0300

    classe de carro

---
+++ dao/db.php
@@ -0,0 +1,8 @@
+<?php
+    $db = "daoteste";
+    $host = "localhost";
+    $user = "root";
+    $password = "Root*3306";
+
+    $connection = new PDO("mysql:dbname=$db;host=$host", $user, $password);
+?>
---
+++ dao/model/Car.php
@@ -0,0 +1,51 @@
+<?php
+    class Car {
+
+        private $id;
+        private $brand;
+        private $km;
+        private $color;
+
+        function __construct($id, $brand, $km, $color){
+            $this -> $id = $id;
+            $this -> $brand = $brand;
+            $this -> $km = $km;
+            $this -> $color = $color;
+        }
+
+        public function getId(){
+            return $this -> $id;
+        }
+
+        public function setId($id){
+            $this -> $id = $id;
+        }
+        public function getbrand(){
+            return $this -> $brand;
+        }
+
+        public function setBrand($brand){
+            $this -> $brand = $brand;
+        }
+
+        public function getKm(){
+            return $this -> $km;
+        }
+
+        public function setkm($km){
+            $this -> $km = $km;
+        }
+
+        public function getColor(){
+            return $this -> $color;
+        }
+
+        public function setcolor($color){
+            $this -> $color = $color;
+        }
+    }
+
+    interface CarDAO{
+        
+    }
+?>
